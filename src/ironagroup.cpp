d13c36be9871b33ce83632b64d7b808c1bc42367
PavanMantripragada <mppavan@umd.edu>
2022-12-08 11:45:06 -0500

    1.11 definition of Irona group class
    implemented constructor for ironagroup class

---
+++ src/ironagroup.cpp
@@ -0,0 +1,57 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2022 Pavan Mantripragada
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * @file ironagroup.cpp
+ * @author Pavan, Tharun, Yash
+ * @brief cpp file for IronaGroup class
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "irona/ironagroup.hpp"
+
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+
+#include <string>
+#include <vector>
+
+/**
+ * @brief Construct a new Irona Group:: Irona Group object
+ *
+ */
+IronaGroup::IronaGroup()
+    : moveBaseClient_("move_base", true),
+      cleanRoomServer_(nh_, "cleanroom",
+                       boost::bind(&IronaGroup::cleanRoom, this, _1), true) {
+  ROS_INFO("Entered the construtor.....");
+  detectObjectClient_ = nh_.serviceClient<irona::DetectObject>("detect_object");
+  deleteClient =
+      nh_.serviceClient<gazebo_msgs::DeleteModel>("/gazebo/delete_model");
+  // start the action server
+  ROS_INFO("CLeanroom service starting....");
+  cleanRoomServer_.start();
+  ROS_INFO("CLeanroom service done.....");
+}
